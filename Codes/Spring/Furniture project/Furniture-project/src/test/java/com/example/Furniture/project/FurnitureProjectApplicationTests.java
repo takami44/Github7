f2b9aabb092b14c6eb952a416839d5c6ce30e672
LaçınYaqubov <148263980+takami44@users.noreply.github.com>
2025-09-14 12:24:30 +0400

    dd
    a

---
+++ Codes/Spring/Furniture project/Furniture-project/src/test/java/com/example/Furniture/project/FurnitureProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Furniture.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
